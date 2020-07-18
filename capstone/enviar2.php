6162f64a0520338ab9a86fcbb98b40cf98f4aeaa
marcemarto <67545296+marcemarto@users.noreply.github.com>
2020-07-18 19:16:52 +0100

    Create enviar2.php

---
+++ capstone/enviar2.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$phone = $_POST['Phone'];
+$subject = $_POST['Subject'];
+$message = $_POST['Message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $name . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['Message'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'ignaciobluuweb@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $subject, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
